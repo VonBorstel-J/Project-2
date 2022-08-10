60bf8f7e011ad6d777f74b50ac4711cb50c953c5
VonBorstel-J <jordanvonborstel@gmail.com>
2022-08-10 16:06:23 -0700

    Created Mongo DB connection and set up start.js

--- start.js
+++ start.js
@@ -1,23 +1,12 @@
-require('dotenv').config(); 
-const mongoose = require('mongoose'); 
 
-mongoose.connect(process.env.DATABASE, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-}); 
-
-mongoose.connection 
-    .on('open', () => { 
-        console.log('Mongoose connection open');
-    })
-    .on('error', (err) => { 
-        console.log(`Connection error: ${err.message}`);
-    });
+require('./initDB')();
 
 require('./models/Registration');
 const app = require('./app'); 
 
-
 const server = app.listen(3000, () => {
     console.log(`Express is running on port ${server.address().port}`);
 }); 
+
+const morgan = require('morgan');
+app.use(morgan('tiny'));
