717ec6997a4ed6bed84cd7655da014005ea3f34c
VonBorstel-J <jordanvonborstel@gmail.com>
2022-08-20 18:26:59 -0700

    ///

--- js/scroll_nav.js
+++ js/scroll_nav.js
@@ -1,4 +1,6 @@
 
+//Change color on scroll
+
 window.onscroll = () => {scrollNavbar()};
 
 scrollNavbar = () => {
@@ -18,10 +20,46 @@ if (document.documentElement.scrollTop > 100) {
 } else {
     navBar.classList.remove("pa-fixed-header");
     
-    // Change the color of links back to default
+    // Changes the color of links back to default
     for (let i = 0; i < links.length; i++) {
         const element = links[i];
         element.classList.remove('text-black');
     }
 }
 }
+
+
+
+
+//Hamburger Menu 
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-links");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+
+
+const navLink = document.querySelectorAll(".nav-links");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+}
+
